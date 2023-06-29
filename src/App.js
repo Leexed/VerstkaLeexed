550507f4808b426ca9b7bdc933543b3b4214dc1a
Leexed <leexedromzov@gmail.com>
2023-06-30 04:21:56 +0700

    2nd page finish

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
 import logo from './logo.svg';
+import './styles/text.scss'
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import CoursesPage from './pages/courses';
 import HomePage from './pages/home';
 import { HeaderPage } from './pages/header';
+import Footer from './pages/footer';
 
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-      <HeaderPage/>
-        <Routes>
-          <Route path={'/'} element={<HomePage/>}/>
-          <Route path={'/courses'} element={<CoursesPage/>}/>
-      </Routes>
+        <HeaderPage/>
+          <Routes>
+            <Route path={'/'} element={<HomePage/>}/>
+            <Route path={'/courses'} element={<CoursesPage/>}/>
+            
+        </Routes>
+        <Footer/>
+    
       </BrowserRouter>
+      
     </div>
   );
 }
